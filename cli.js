601a8026edab802ff2684da08cc6426385dc996c
Gabriel Farrell <g@grrawr.com>
2014-01-03 11:38:55 -0500

    Hacky proxy for instant browser reload

--- cli.js
+++ cli.js
@@ -3,6 +3,7 @@ var detective = require('detective')
 var fs = require('fs')
 var fork = require('child_process').fork
 var glob = require('glob')
+var http = require('http')
 var logstamp = require('logstamp')
 var resolve = require('resolve')
 var filewatcher = require('filewatcher')
@@ -11,10 +12,22 @@ var watchify = require('watchify')
 // Timestamp logs
 logstamp(function () {return new Date().toISOString() + ' [deva] '})
 
+var port = process.env.PORT || 1028;
+var childPort = Math.floor(Math.random()*(Math.pow(2,16)-1024)+1024)
+var childEnv = process.env
+childEnv.PORT = childPort
+
+// TODO Pool for multiple sseRes -- only one connection gets responses now
+var sseRes;
+
 var child = {}
 function startServer () {
   console.log('Starting server.js')
-  child = fork('server.js', {env: process.env})
+  child = fork('server.js', {env: childEnv})
+  child.on('message', function (m) {
+    console.log('message received:', m)
+    if (m === 'online' && sseRes) sseRes.write('data: reload\n\n')
+  })
 }
 
 var w = watchify('./browser/main.js')
@@ -91,8 +104,31 @@ bundle(function () {
   startServer()
 })
 
+// Reload on any console input
 process.stdin.resume()
-process.stdin.setEncoding('utf8')
 process.stdin.on('data', function (chunk) {
   restart()
 })
+
+// Hacky proxy
+var server = http.createServer(function(req, res) {
+  if (RegExp('^/_sse').test(req.url)) {
+    res.setHeader('Content-Type', 'text/event-stream')
+    res.setHeader('Cache-Control', 'no-cache')
+    res.setHeader('Connection', 'close')
+    sseRes = res
+  } else {
+    var clientReq = http.request({
+      port: childPort,
+      method: req.method,
+      path: req.url,
+      headers: req.headers
+    }, function (clientRes) {
+      res.writeHead(clientRes.statusCode, clientRes.headers)
+      // TODO Insert SSE reload <script> in HTML responses
+      clientRes.pipe(res)
+    })
+    req.pipe(clientReq)
+  }
+})
+server.listen(port)
