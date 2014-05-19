996342ea5fdaffc6ee32d8215aa4f9f31172b5a4
Gabriel Farrell <g@grrawr.com>
2014-05-19 12:09:07 -0400

    Listen for online rather than setting timeout

--- test/no-devarc/main-test.js
+++ test/no-devarc/main-test.js
@@ -1,6 +1,6 @@
-var test = require('../wrapper')(__dirname)
 var fs = require('fs')
 var http = require('http')
+var test = require('../wrapper')(__dirname)
 
 
 test('server process responds to requests', function (t) {
@@ -16,27 +16,26 @@ test('server process responds to requests', function (t) {
 test('modification of required file reloads child process', function (t) {
   t.plan(1)
   fs.writeFileSync(__dirname + '/res.js', 'module.exports = function (res) {\n  res.end(\'okay\')\n}')
-
-  // Give server time to reload
-  setTimeout(function () {
+  test.server.once('online', function () {
     http.get({port: 1028}, function (res) {
       res.on('data', function (data) {
         t.equal(data.toString(), 'okay', 'okay on response')
       })
     })
-  }, 1000)
+  })
 })
 
 test('return file to original state', function (t) {
   t.plan(1)
-  fs.writeFileSync(__dirname + '/res.js', 'module.exports = function (res) {\n  res.end(\'ok\')\n}')
-
-  // Give server time to reload
+  // Pause longer than watch interval before writing to file again
   setTimeout(function () {
+    fs.writeFileSync(__dirname + '/res.js', 'module.exports = function (res) {\n  res.end(\'ok\')\n}')
+  }, 1000)
+  test.server.once('online', function () {
     http.get({port: 1028}, function (res) {
       res.on('data', function (data) {
         t.equal(data.toString(), 'ok', 'ok on response')
       })
     })
-  }, 500)
+  })
 })
