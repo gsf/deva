19b5385ae3cd953889af52289319326f74ecd8d8
Gabriel Farrell <g@grrawr.com>
2014-06-30 16:07:22 -0400

    These test pauses no longer needed

--- test/cat-files/main-test.js
+++ test/cat-files/main-test.js
@@ -15,10 +15,7 @@ test('server process responds to requests', function (t) {
 
 test('modifying script file cats to static and reloads child process', function (t) {
   t.plan(1)
-  // Pause for command rerun interval
-  setTimeout(function () {
-    fs.writeFileSync(__dirname + '/files/b.txt', '2')
-  }, 1000)
+  fs.writeFileSync(__dirname + '/files/b.txt', '2')
   test.server.once('online', function () {
     http.get({port: 1028}, function (res) {
       res.on('data', function (data) {
@@ -30,10 +27,7 @@ test('modifying script file cats to static and reloads child process', function 
 
 test('return script to original state', function (t) {
   t.plan(1)
-  // Pause for command rerun interval
-  setTimeout(function () {
-    fs.writeFileSync(__dirname + '/files/b.txt', '1')
-  }, 1000)
+  fs.writeFileSync(__dirname + '/files/b.txt', '1')
   test.server.once('online', function () {
     http.get({port: 1028}, function (res) {
       res.on('data', function (data) {
